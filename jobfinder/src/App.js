0dffbfc9e40b07313126d6cef4daeef62030fcb6
Mehar-Aziz <meharaziz.1614>
2024-08-05 20:20:05 +0500

    Modified App.js File

---
+++ jobfinder/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+
+function App() {
+  return(
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+export default App;

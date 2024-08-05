413148ce9f8a7d4e9d9b236bb3ce1931ed25ef9f
Mehar-Aziz <meharaziz.1614>
2024-08-05 23:08:42 +0500

    Footer

--- jobfinder/src/App.js
+++ jobfinder/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return(
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route />
         </Routes>
+        <Footer />
       </div>
     </Router>
   )

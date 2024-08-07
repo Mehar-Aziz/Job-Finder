0f13194fad37e455548b57fd8412ea2e25357211
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-08-08 00:31:51 +0500

    Hero Image

--- jobfinder/src/components/Slider.jsx
+++ jobfinder/src/components/Slider.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Select from 'react-select';
+import heroImage from '../assets/images/hero.jpg'
 
 const locationOptions = [
     { value: 'bd', label: 'Location BD' },
@@ -11,8 +12,9 @@ const locationOptions = [
 const customStyles = {
     control: (provided) => ({
         ...provided,
-        margin: '10px 0',
-        paddingTop: '8px',
+        margin: '0px 0',
+        paddingTop: '17px',
+        paddingBottom: '17px',
         border: 'none',
     }),
     menu: (provided) => ({
@@ -24,7 +26,12 @@ const customStyles = {
 const Slider = () => (
   <div className="slider-area">
     <div className="slider-active">
-      <div className="single-slider slider-height d-flex align-items-center" data-background="assets/img/hero/h1_hero.jpg">
+      <div className="single-slider slider-height d-flex align-items-center" style={{
+          backgroundImage: `url(${heroImage})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
         <div className="container">
           <div className="row">
             <div className="col-xl-6 col-lg-9 col-md-10">

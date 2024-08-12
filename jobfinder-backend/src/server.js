b9e23e79ae55bac885620c4370abce3ab42e3b73
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-08-12 12:31:09 +0500

    Server js

--- jobfinder-backend/src/server.js
+++ jobfinder-backend/src/server.js
@@ -2,3 +2,17 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db';
+import userRoutes from './routes/userRoutes';
+
+dotenv.config();
+connectDB();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/users', router);
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

678c3b3e06532756087cfcd6c7632abdeadd526d
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-08-12 17:31:26 +0500

    Registration Authorization

--- jobfinder/src/pages/Registration.jsx
+++ jobfinder/src/pages/Registration.jsx
@@ -11,7 +11,7 @@ const RegistrationForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:3000/api/register' , { name , email, password });
+      await axios.post(`http://localhost:3000/api/registration`, { name , email, password });
       alert('Registraion Successful!');
     } catch (error) {
       setError(error.response.data.message);
@@ -41,7 +41,7 @@ const RegistrationForm = () => {
               id="email"
               name="email"
               value={email}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
           </div>

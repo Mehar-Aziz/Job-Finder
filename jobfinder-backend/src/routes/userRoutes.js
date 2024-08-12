3631dae2c0b99183ecc2c02bc489057aa53a3b9c
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-08-12 15:53:22 +0500

    Registration router

--- jobfinder-backend/src/routes/userRoutes.js
+++ jobfinder-backend/src/routes/userRoutes.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import { getUser, createUser } from '../controllers/userController.js';
+import { register } from '../controllers/userController.js';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.route('/').get(getUser).post(createUser);
+router.post('/registration', [
+    body('name').notEmpty().withMessage('Name is Required'),
+    body('email').isEmail().withMessage('Valid email is required'),
+    body('password').isLength({ min:6 }).withMessage('Password must be at least 6 characters')
+], register);
 
 export default router;

1c2cb0f77520a5c8a5ca936f3c57e06f5eb04888
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-08-12 15:43:14 +0500

    Authentication Register controller

--- jobfinder-backend/src/controllers/userController.js
+++ jobfinder-backend/src/controllers/userController.js
@@ -1,31 +1,23 @@
 import User from "../models/userModel.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { validationResult } from 'express-validator';
 
-//get all users
-export const getUser = async (req, res) => {
-    try {
-        const users = await User.find();
-        res.json(users);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+//registration
+
+exports.register = async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
     }
-};
-
-//create a new user
-export const createUser = async (req, res) => {
     const { name, email, password } = req.body;
-
-    const newUser = new User({
-        name,
-        email,
-        password,
-    });
-
     try {
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        res.status(201).json({ message: 'User Registered Successfully' });
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        res.status(500).json({ message: 'Server error' });
     }
 };
-
 

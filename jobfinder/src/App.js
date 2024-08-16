bae66aea7d946b2f9d7ef855850da4f7c10d0435
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-08-16 14:11:58 +0500

    Forget Password Implementation

--- jobfinder/src/App.js
+++ jobfinder/src/App.js
@@ -14,6 +14,7 @@ import RegistrationForm from "./pages/Registration";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
 import AuthProvider from "./context/UserContext";
+import ForgotPassword from "./pages/ForgetPassword";
 
 
 function App() {
@@ -45,6 +46,7 @@ function App() {
         <Routes>
           <Route path="/" exact Component={Home}  />
           <Route path="/login" Component={LoginPage} />
+          <Route path="/forgot-password" Component={ForgotPassword} />
           <Route path="/dashboard" Component={Dashboard} />
           <Route path="/profile" Component={Profile} />
           <Route path="/registration" Component={RegistrationForm} />
---
+++ jobfinder/src/pages/ForgetPassword.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ForgotPassword from '../components/ForgetPasswordForm';
+import heroImage from '../assets/images/contact.jpg'
+
+const ForgotPassword = () => {
+  return (
+    <>
+    {/* Hero Area Start */}
+    <div className="slider-area">
+    <div
+      className="single-slider section-overly slider-height2 d-flex align-items-center"
+      style={{ backgroundImage: `url(${heroImage})` }}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-12">
+            <div className="hero-cap text-center">
+              <h2>Forget Password</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+   <ForgotPassword />
+   </>
+  );
+};
+
+export default ForgotPassword ;

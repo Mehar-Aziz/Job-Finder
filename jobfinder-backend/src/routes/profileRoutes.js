91730d3a03dfa1f100873309859c7071b877b4bd
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-08-17 14:54:43 +0500

    Update profile route

--- jobfinder-backend/src/routes/profileRoutes.js
+++ jobfinder-backend/src/routes/profileRoutes.js
@@ -1,12 +1,10 @@
 import express from 'express';
-
 import { getProfile, updateProfile } from '../controllers/profileController';
+
 const router = express.Router();
 
 
-router.get('/:userId', getProfile);
+router.get('/profile/:id', getProfile);
+router.put('/profile/:id', updateProfile);
 
-
-router.put('/:userId', updateProfile);
-
-module.exports = router;
+export default router;

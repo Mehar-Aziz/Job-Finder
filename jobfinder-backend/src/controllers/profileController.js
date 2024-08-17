9068c93232747165f94649b228c7e79c2447e2f9
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-08-17 14:52:06 +0500

    Edit Controller

--- jobfinder-backend/src/controllers/profileController.js
+++ jobfinder-backend/src/controllers/profileController.js
@@ -1,21 +1,34 @@
 import Profile from "../models/profileSchema";
 
-export const getProfile = async (req, res) => {
-  try {
-    const user = await Profile.findById(req.params.userId);
-    if (!user) return res.status(404).json({ message: 'User not found' });
-    res.json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+export const getUserProfile = async (req, res) => {
+    try {
+      const user = await Profile.findById(req.params.id);
+      if (!user) {
+        return res.status(404).json({ msg: 'User not found' });
+      }
+      res.json(user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ msg: 'Server error' });
+    }
+  };
+  
 
-export const updateProfile = async (req, res) => {
-  try {
-    const user = await Profile.findByIdAndUpdate(req.params.userId, req.body, { new: true });
-    if (!user) return res.status(404).json({ message: 'User not found' });
-    res.json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+  export const updateUserProfile = async (req, res) => {
+    try {
+      const updatedUser = await Profile.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true, runValidators: true }
+      );
+  
+      if (!updatedUser) {
+        return res.status(404).json({ msg: 'User not found' });
+      }
+  
+      res.json(updatedUser);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ msg: 'Server error' });
+    }
+  };

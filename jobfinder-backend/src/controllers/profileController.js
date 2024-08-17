5a9caa28d27d3f703d092513a15f642e6a181693
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-08-17 14:39:02 +0500

    Profile controller

---
+++ jobfinder-backend/src/controllers/profileController.js
@@ -0,0 +1,21 @@
+import User from "../models/profileSchema";
+
+export const getProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.userId, req.body, { new: true });
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};

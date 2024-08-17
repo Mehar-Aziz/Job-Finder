faa987bd8649421bf730ada9b89f83ed20005c1a
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-08-17 14:58:18 +0500

    Profile link server

--- jobfinder-backend/src/server.js
+++ jobfinder-backend/src/server.js
@@ -6,10 +6,13 @@ import userRoutes from './routes/userRoutes.js';
 import serviceRoutes from './routes/serviceRoute.js';
 import testimonialRoutes from './routes/testimonalRoutes.js';
 import contactRoutes from './routes/contactRoutes.js';
+import profileRoutes from './routes/profileRoutes.js';
 
 dotenv.config();
 connectDB();
 const app = express();
+
+app.use(express.json({ extended: false }));
 
 const corsOptions = {
     origin: 'http://localhost:3001', 
@@ -26,6 +29,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/services', serviceRoutes);
 app.use('/api', testimonialRoutes);
 app.use('/api/contact', contactRoutes);
+app.use('/api', profileRoutes);
 
 
 const PORT = 3000;
